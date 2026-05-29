33863ac11cd33687735526992cd0bf3a01eff897
Tu Bui <bdtu5874@gmail.com>
2026-05-29 21:59:15 +0700

    fix typo

--- seabreeze/src/api.js
+++ seabreeze/src/api.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-// Tạo một instance của axios với cấu hình sẵn
 const api = axios.create({
-  baseURL: 'https://seabreeze-backend-wkqw.onrender.com/', // Nhớ kiểm tra kỹ có cần '/api' hay không
+  // Lấy từ biến môi trường, nếu không có thì mặc định về localhost cho m test local
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000', 
   headers: {
-    'Content-Type': 'appligit cation/json',
+    'Content-Type': 'application/json', 
   },
-  
 });
 
-// Xuất ra để các file khác sử dụng
 export default api;
