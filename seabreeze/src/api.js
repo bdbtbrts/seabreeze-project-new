fff512a50d52ea93065ecfa47d3ce550f2cc58d9
Tu Bui <bdtu5874@gmail.com>
2026-05-29 22:07:34 +0700

    bao dong gia

--- seabreeze/src/api.js
+++ seabreeze/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Lấy từ biến môi trường, nếu không có thì mặc định về localhost cho m test local
+  // Lấy từ biến môi trường, nếu không có thì mặc định về localhost cho m test  local
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000', 
   headers: {
     'Content-Type': 'application/json', 
