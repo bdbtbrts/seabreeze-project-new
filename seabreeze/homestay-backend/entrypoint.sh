ecee646e3c7291e4716f38c8a3432c9a8a3825a1
Tu Bui <bdtu5874@gmail.com>
2026-05-29 22:27:56 +0700

    fix entrypoint

--- seabreeze/homestay-backend/entrypoint.sh
+++ seabreeze/homestay-backend/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#!/bin/sh
 # Chạy migrate và clear cache
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
+php artisan migrate --force
 
 # Chạy apache
 exec apache2-foreground
+apache2-foreground
