aba466ef0f9b2c65c7462f65045b020920e65c65
Tu Bui <bdtu5874@gmail.com>
2026-05-29 17:02:19 +0700

    Switch to VITE_PROD_URL for backend

--- seabreeze/src/api.js
+++ seabreeze/src/api.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 
 // M thử console.log ra xem nó là cái gì khi chạy trên Vercel
 console.log("URL từ môi trường:", import.meta.env.VITE_API_URL);
-
+console.log("DEBUG - VITE_API_URL hiện tại là:", import.meta.env.VITE_API_URL);
 const api = axios.create({
-  // Xóa bỏ cái phần '|| localhost' đi để xem nó có lấy được biến không
-  // Hoặc viết rõ ràng như này để bắt nó dùng biến VITE_API_URL
-  baseURL: import.meta.env.VITE_API_URL || 'https://seabreeze-backend-wkqw.onrender.com',
+  baseURL: import.meta.env.VITE_PROD_URL || 'https://seabreeze-backend-wkqw.onrender.com',
 });
 
 export default api;
