9e038798883f4709bcc41f0198b94446131e04e7
Tu Bui <bdtu5874@gmail.com>
2026-05-29 16:19:03 +0700

    Fix API URL

--- seabreeze/src/api.js
+++ seabreeze/src/api.js
@@ -1,7 +1,13 @@
+// file api.js
 import axios from 'axios';
 
+// M thử console.log ra xem nó là cái gì khi chạy trên Vercel
+console.log("URL từ môi trường:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  // Xóa bỏ cái phần '|| localhost' đi để xem nó có lấy được biến không
+  // Hoặc viết rõ ràng như này để bắt nó dùng biến VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL || 'https://seabreeze-backend-wkqw.onrender.com',
 });
 
 export default api;
